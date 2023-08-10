aaa25fd1d479a9b6c9086717d8d206ace8c2a48d
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-10 15:36:03 +1000

    feat: add capturing functionality

--- internal/mancala/board_side.go
+++ internal/mancala/board_side.go
@@ -21,8 +21,8 @@ func NewBoardSide(player *Player) *BoardSide {
 	return side
 }
 
-// Returns the number of stones that were left over after the move and a boolean to represent whether the player gets another turn.
-func (side *BoardSide) ExecuteMove(pitIndex int) (int, bool) {
+// Returns: the number of stones that were left over after the move, whether the player gets another turn, and the pit index the player is capturing from (-1 for no capture)
+func (side *BoardSide) ExecuteMove(pitIndex int) (int, bool, int) {
 	ValidatePitIndex(pitIndex)
 
 	stones := side.Pits[pitIndex]
@@ -30,22 +30,30 @@ func (side *BoardSide) ExecuteMove(pitIndex int) (int, bool) {
 	side.Pits[pitIndex] = 0
 
 	for i := pitIndex; i < PitsPerSide && stones > 0; i++ {
+		// If one stone left and the next pit is empty, capture
+		if stones == 1 && side.Pits[i] == 0 {
+			// Add one for our stone
+			side.Store++
+			// Return the pit index (of the opponents board side) we want to capture from
+			return 0, false, (PitsPerSide - 1) - i
+		}
+
 		side.Pits[i]++
 		stones--
 	}
 
 	switch {
 	case stones == 0:
-		return 0, false
+		return 0, false, -1
 	case stones == 1:
 		side.Store++
-		return 0, true
+		return 0, true, -1
 	case stones > 1:
 		side.Store++
-		return stones - 1, false
+		return stones - 1, false, -1
 	default:
 		log.Error().Msgf("Invalid number of stones: %d", stones)
-		return 0, false
+		return 0, false, -1
 	}
 }
 
@@ -65,4 +73,11 @@ func (side *BoardSide) GetScore() int {
 		score += pit
 	}
 	return score + side.Store
+}
+
+// Empties the pit and returns the number of stones in it
+func (side *BoardSide) Capture(pitIndex int) int {
+	stones := side.Pits[pitIndex]
+	side.Pits[pitIndex] = 0
+	return stones
 }
--- internal/mancala/game.go
+++ internal/mancala/game.go
@@ -17,14 +17,26 @@ func (g *Game) ExecuteMove(pitIndex int) {
 		currentSide = g.Side2
 	}
 
-	var stones int
+	var stones, captureIndex int
 	var extraTurn bool
 	// Do while there are stones left to distribute
 	for i := true; i; i = stones > 0 {
-		stones, extraTurn = currentSide.ExecuteMove(pitIndex)
+		stones, extraTurn, captureIndex = currentSide.ExecuteMove(pitIndex)
 
 		// If we have finished distributing stones
 		if stones <= 0 {
+			// If they get to capture
+			if captureIndex >= 0 {
+				// Get the other board side
+				var otherSide *BoardSide
+				if g.Turn == Player1Turn {
+					otherSide = g.Side2
+				} else {
+					otherSide = g.Side1
+				}
+				// Add captured stones to store
+				currentSide.Store += otherSide.Capture(captureIndex)
+			}
 			// If they don't get to take another turn, alternate the turn
 			if !extraTurn {
 				g.AlternateTurn()
