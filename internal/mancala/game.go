de4e97c9a78a6ff04bab51f30d2171c2324658a0
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-11 12:56:01 +1000

    refactor: move NewGame function to game.go

--- internal/mancala/game.go
+++ internal/mancala/game.go
@@ -6,6 +6,19 @@ type Game struct {
 	Turn  uint8
 }
 
+const (
+	Player1Turn = 1
+	Player2Turn = 2
+)
+
+func NewGame(player1 *Player, player2 *Player, stonesPerPit int, pitsPerSide int) *Game {
+	return &Game{
+		Side1: NewBoardSide(player1, stonesPerPit, pitsPerSide),
+		Side2: NewBoardSide(player2, stonesPerPit, pitsPerSide),
+		Turn:  Player1Turn,
+	}
+}
+
 func (g *Game) ExecuteMove(pitIndex int) {
 	// Get correct board side
 	var currentSide *BoardSide
