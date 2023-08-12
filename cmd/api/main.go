3b908d9120989e21e541b7d54cbea8622b993448
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-12 15:15:14 +1000

    feat: remove external dependencies chi, zerolog, pflag; add slog logging

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/rs/zerolog/log"
-	flag "github.com/spf13/pflag"
+	log "golang.org/x/exp/slog"
 )
 
 func main() {
-	addr := flag.StringP("addr", "a", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 
 	flag.Parse()
 
 	err := run(addr)
 	if err != nil {
-		log.Fatal().Err(err).Send()
+		log.Error(err.Error())
 	}
 }
 
 func run(address *string) error {
-	router := chi.NewRouter()
+	mux := http.NewServeMux()
 
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("PLACEHOLDER"))
 		if err != nil {
-			log.Err(err).Send()
+			log.Error(err.Error())
 		}
 	})
 
-	err := http.ListenAndServe(*address, router)
+	log.Info("Starting server", log.String("addr", *address))
+	err := http.ListenAndServe(*address, mux)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -2,14 +2,4 @@ module github.com/Broderick-Westrope/mancala-go
 
 go 1.20
 
-require (
-	github.com/go-chi/chi/v5 v5.0.10
-	github.com/rs/zerolog v1.30.0
-	github.com/spf13/pflag v1.0.5
-)
-
-require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-)
+require golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb
