c6ccc63f5a4ec9e36cb537821468a5e56ac9d413
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-14 08:36:14 +1000

    feat: add keymap using github.com/charmbracelet/bubbles/key

---
+++ internal/tui/keys/keys.go
@@ -0,0 +1,62 @@
+package keys
+
+import "github.com/charmbracelet/bubbles/key"
+
+type KeyMap struct {
+	Up        key.Binding
+	Down      key.Binding
+	Left      key.Binding
+	Right     key.Binding
+	Submit    key.Binding
+	Help      key.Binding
+	Quit      key.Binding
+	ForceQuit key.Binding
+}
+
+func (k KeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.ForceQuit}
+}
+
+func (k KeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down},
+		{k.Left, k.Right},
+		{k.Submit, k.Quit},
+		{k.Help, k.ForceQuit},
+	}
+}
+
+var Keys = KeyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k", "w"),
+		key.WithHelp("↑/k/w", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j", "s"),
+		key.WithHelp("↓/j/s", "move down"),
+	),
+	Left: key.NewBinding(
+		key.WithKeys("left", "h", "a"),
+		key.WithHelp("←/h/a", "move left"),
+	),
+	Right: key.NewBinding(
+		key.WithKeys("right", "l", "d"),
+		key.WithHelp("→/l/d", "move right"),
+	),
+	Submit: key.NewBinding(
+		key.WithKeys(" ", "enter"),
+		key.WithHelp("enter/space", "submit selection"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc"),
+		key.WithHelp("q/escape", "quit"),
+	),
+	ForceQuit: key.NewBinding(
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("Ctrl+C", "force quit"),
+	),
+}
