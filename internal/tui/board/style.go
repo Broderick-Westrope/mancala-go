fa85d0cc69981eb9be8c7532ac3878df51401569
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-17 15:23:16 +1000

    style: change the selected color to magenta

--- internal/tui/board/style.go
+++ internal/tui/board/style.go
@@ -4,6 +4,6 @@ import "github.com/charmbracelet/lipgloss"
 
 var (
 	baseStyle      = lipgloss.NewStyle().Padding(0, 1)
-	renderSelected = baseStyle.Copy().Foreground(lipgloss.Color("3")).Render
+	renderSelected = baseStyle.Copy().Foreground(lipgloss.Color("5")).Bold(true).Render
 	renderDisabled = baseStyle.Copy().Foreground(lipgloss.Color("8")).Render
 )
