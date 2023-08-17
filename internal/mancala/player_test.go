32bfde72f4073ce8e289af1ec78b8161578ed66a
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-17 16:59:08 +1000

    test: add Human method tests

--- internal/mancala/player_test.go
+++ internal/mancala/player_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Broderick-Westrope/mancala-go/internal/mancala"
 )
 
-func TestNewPlayer(t *testing.T) {
+func TestNewHuman(t *testing.T) {
 	tests := []struct {
 		name          string
 		playerName    string
@@ -21,6 +21,58 @@ func TestNewPlayer(t *testing.T) {
 			ans := mancala.NewHuman(tt.playerName)
 			checkEquals(t, "Name", ans.Name, tt.expectedName)
 			checkEquals(t, "Score", ans.Score, tt.expectedScore)
+		})
+	}
+}
+
+func TestHuman_GetName_GetScore(t *testing.T) {
+	tests := []struct {
+		name          string
+		human         *mancala.Human
+		expectedName  string
+		expectedScore int
+	}{
+		{
+			"Valid name",
+			&mancala.Human{
+				Name:  "Test Name 1",
+				Score: 5,
+			},
+			"Test Name 1",
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			name := tt.human.GetName()
+			score := tt.human.GetScore()
+			checkEquals(t, "Name", name, tt.expectedName)
+			checkEquals(t, "Score", score, tt.expectedScore)
+		})
+	}
+}
+
+func TestHuman_SetScore(t *testing.T) {
+	tests := []struct {
+		name          string
+		human         *mancala.Human
+		expectedScore int
+	}{
+		{
+			"Valid score",
+			&mancala.Human{
+				Name:  "Test Name 1",
+				Score: 5,
+			},
+			12,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.human.SetScore(tt.expectedScore)
+			checkEquals(t, "Score", tt.human.Score, tt.expectedScore)
 		})
 	}
 }
