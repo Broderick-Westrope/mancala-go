9e074268e1c6bf4f6a2356fb10e84279cb2697a9
Broderick Westrope <broderickwestrope@gmail.com>
2023-08-21 10:15:45 +1000

    docs(mancala): add package comment

---
+++ internal/mancala/doc.go
@@ -0,0 +1,6 @@
+/*
+mancala is a library for the game of Mancala.
+It includes the core game logic and added functionality like algorithmic opponents.
+It is designed to be used by a UI, such as the tui package in the same repository.
+*/
+package mancala
